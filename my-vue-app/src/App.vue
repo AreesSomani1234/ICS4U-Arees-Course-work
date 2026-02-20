aac6c77a377a53bd6bc26a1dc873142206a84ca4
Arees Somani <asomani@bayviewglen.ca>
2026-02-20 10:39:43 -0500

    changes

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{ product }}</h1>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const product = ref('Socks')
+const description = ref('Warm, comfortable socks for everyday wear.')
+</script>
+
