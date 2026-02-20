f0c419a6b913a649b5613bfbaac73c248642d471
Arees Somani <asomani@bayviewglen.ca>
2026-02-20 11:27:49 -0500

    changes

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -2,13 +2,37 @@
   <div>
     <h1>{{ product }}</h1>
     <p>{{ description }}</p>
+
+    <div class="product-image">
+      <img v-bind:src="image" :title="product">
+      <img :src="image" :title="product">  <!-- shorthand -->
+  
+    </div>
+
+    <button @click="switchToBlue()">Switch to Blue Socks</button>
+    <button @click="switchToGreen()">Switch to Green Socks</button>
   </div>
+  <a :href="url" target="_blank" rel="noopener">Open product link</a>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+const url = ref('https://www.nike.com')
+
+import socksGreen from './assets/images/green_socks.jpg'
+import socksBlue from './assets/images/bluesocks.jpg'
+
 
 const product = ref('Socks')
 const description = ref('Warm, comfortable socks for everyday wear.')
+const image = ref(socksGreen)
+
+function switchToBlue() {
+  image.value = socksBlue
+}
+function switchToGreen(){
+  image.value = socksGreen
+}
 </script>
+
 
