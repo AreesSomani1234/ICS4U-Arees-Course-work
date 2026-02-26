7a73f9e76a4ad30b489f081a77d0fb313ae8561f
Arees Somani <asomani@bayviewglen.ca>
2026-02-26 10:41:38 -0500

    changes less 5

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -3,36 +3,61 @@
     <h1>{{ product }}</h1>
     <p>{{ description }}</p>
 
-    <div class="product-image">
-      <img v-bind:src="image" :title="product">
-      <img :src="image" :title="product">  <!-- shorthand -->
-  
-    </div>
+    <!-- Product image (changes on hover) -->
+    <img :src="image" :alt="product" />
 
-    <button @click="switchToBlue()">Switch to Blue Socks</button>
-    <button @click="switchToGreen()">Switch to Green Socks</button>
+    <h3>Variant Colors</h3>
+    <ul>
+      <li
+        v-for="variant in variants"
+        :key="variant.id"
+        @mouseover="updateImage(variant.image)"
+      >
+        {{ variant.color }}
+      </li>
+    </ul>
+
+    <h3>Sizes</h3>
+    <ul>
+      <li v-for="size in sizes" :key="size">{{ size }}</li>
+    </ul>
+
+    <p>Cart: {{ cart }}</p>
+
+    <button @click="addToCart">Add To Cart</button>
+    <button @click="removeFromCart">Remove from Cart</button>
   </div>
-  <a :href="url" target="_blank" rel="noopener">Open product link</a>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-const url = ref('https://www.nike.com')
 
-import socksGreen from './assets/images/green_socks.jpg'
-import socksBlue from './assets/images/bluesocks.jpg'
-
+// ✅ Import images from src/assets (Vite requires this)
+import greenSocks from './assets/images/green_socks.jpg'
+import blueSocks from './assets/images/bluesocks.jpg'
 
 const product = ref('Socks')
 const description = ref('Warm, comfortable socks for everyday wear.')
-const image = ref(socksGreen)
+const sizes = ref(['S', 'M', 'L', 'XL'])
 
-function switchToBlue() {
-  image.value = socksBlue
+const cart = ref(0)
+
+const image = ref(greenSocks) // default image
+
+const variants = ref([
+  { id: 2234, color: 'green', image: greenSocks },
+  { id: 2235, color: 'blue', image: blueSocks }
+])
+
+function addToCart() {
+  cart.value += 1
 }
-function switchToGreen(){
-  image.value = socksGreen
+
+function removeFromCart() {
+  cart.value -= 1
+}
+
+function updateImage(variantImage) {
+  image.value = variantImage
 }
 </script>
-
-
